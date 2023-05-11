b094820982a637cf4c74c94a6eff84c2e3f4042b
zonin <business.kariukimartin@gmail.com>
2023-05-11 06:10:27 +0300

    vertex shader file

--- main.cpp
+++ main.cpp
@@ -4,12 +4,7 @@
 #include <cmath>
 
 // vertext shader source code
-const char *vertexShaderSource = "#version 330 core\n"
-                                 "layout (location = 0) in vec3 aPos;\n"
-                                 "void main()\n"
-                                 "{\n"
-                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                 "}\0";
+
 // Fragment Shader source code
 const char *fragmentShaderSource = "#version 330 core\n"
                                    "out vec4 FragColor;\n"
