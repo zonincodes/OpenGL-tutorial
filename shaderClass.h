df14671d6160d3fe2dc89864d74ae38f5dff4446
zonin <business.kariukimartin@gmail.com>
2023-05-11 08:27:10 +0300

    shaderClass.h

--- shaderClass.h
+++ shaderClass.h
@@ -5,6 +5,17 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <cerrno>
 
+std::string get_file_contents(const char* filename);
 
+class Shader
+{
+    public:
+        GLuint ID;
+        Shader(const char* vertexFile, const char* fragmentFile);
+
+        void Activate();
+        void Delete();
+};
 #endif
