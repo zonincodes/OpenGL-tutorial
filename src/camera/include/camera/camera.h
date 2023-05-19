fa7754cb5182df82c81294cf127276923d1aef46
zonin <business.kariukimartin@gmail.com>
2023-05-19 17:47:27 +0300

    camera declaration and defination

--- src/camera/include/camera/camera.h
+++ src/camera/include/camera/camera.h
@@ -9,6 +9,26 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/vector_angle.hpp>
 #include <glm/gtx/rotate_vector.hpp>
+#include <shader/shaderClass.h>
+
+class Camera
+{
+    public:
+        glm::vec3 Position;
+        glm::vec3 Orientation = glm::vec3(0.0f, 0.0f, -1.0f);
+        glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
+
+        int width;
+        int height;
+
+        float speed = 0.1f;
+        float sensitivity = 100.0f;
+
+        Camera(int width, int height, glm::vec3 position);
+
+        void Matrix(float FOVdeg, float nearPlane, float farPlane, Shader &shader, const char *uniform);
+        void Inputs(GLFWwindow *window);
+};
 
 
 #endif
--- src/camera/src/camera.cpp
+++ src/camera/src/camera.cpp
@@ -1,1 +1,19 @@
 #include "camera/camera.h"
+
+Camera::Camera(int width, int height, glm::vec3 position)
+{
+      Camera::width = width;
+      Camera::height = height;
+      Position = position;
+}
+
+void Camera::Matrix(float FOVdeg, float nearPlane, float farPlane, Shader &shader, const char *uniform)
+{
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 projection = glm::mat4(1.0f);
+
+    view = glm::lookAt(Position, Position + Orientation, Up);
+    projection = glm::perspective(glm::radians(FOVdeg), (float)(width/height), nearPlane, farPlane);
+
+    glUniformMatrix4fv(glGetUniformLocation(shader.ID, uniform), 1, GL_FALSE, glm::value_ptr(projection * view));
+}
